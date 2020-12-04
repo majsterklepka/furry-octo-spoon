22e67a5281b93fcbc940e059c91452da81d03053
Paweł Sobótka <48721262935pl@gmail.com>
2020-12-04 18:01:34 +0100

    updated file
    update device clk frequency

--- blinking-led/blink-led.c
+++ blinking-led/blink-led.c
@@ -2,38 +2,37 @@
  * @file blink-led.c
  * @brief avr-gcc example blinking led
  * 
- * @date 2020-09-19
+ * @date 2020-12-04
  * @license GNU GPLv3
  * @author Paweł Sobótka
  *
  */
 
 #ifndef F_CPU
-#define F_CPU 4000000L
+#define F_CPU 1000000L
 #endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 /*
- * F_CPU == 4000000 Hz
- * CKDIV8=1
- * delay = delay/8;
- * real F_CPU = 500000 Hz
+ * F_CPU      = 8000000 Hz
+ * CKDIV8     = 1
+ * real F_CPU = 1000000 Hz
  *
  */
 
 
 int main(void)
 {
-	DDRB |= (1 << DDB0);
+	DDRB |= _BV(DDB0);
 
 	while(1)
 	{
-		PORTB |= (1 << PB0);
-		_delay_ms(125);
-		PORTB &= ~(1 << PB0);
-		_delay_ms(125);
+		PORTB |= _BV(0);
+		_delay_ms(500);
+		PORTB &= ~_BV(0);
+		_delay_ms(500);
 	}
 
 	return 0;
