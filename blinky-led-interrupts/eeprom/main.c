9f24474cd7988b4c78c884fc93d92926367386a8
Paweł Sobótka <48721262935pl@gmail.com>
2021-04-24 10:04:44 +0200

    update

--- blinky-led-interrupts/eeprom/main.c
+++ blinky-led-interrupts/eeprom/main.c
@@ -1,3 +1,18 @@
+/*!
+ * @file main.c
+ * @brief eeprom code for AVR
+ *
+ * @author Pawel Sobotka
+ * @license GNU GPLv3
+ * @copyright (c)2021 Pawel Sobotka, all rights reserved
+ *
+ * @date 2021-04-24
+ *
+ *
+ */
+
+
+
 #ifndef F_CPU
 #define F_CPU 	1000000UL
 #endif
